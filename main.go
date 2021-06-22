bfb4cc02e2f4cd43befb1135227d66c1cae6f27a
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2021-06-22 21:10:44 +0530

    Add: database & models

--- main.go
+++ main.go
@@ -4,10 +4,13 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/mrinjamul/gin-bookstore/models"
 )
 
 func main() {
 	r := gin.Default()
+
+	models.ConnectDataBase()
 
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"message": "hello world"})
