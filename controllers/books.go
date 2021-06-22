6311f067de2e27cd54e235a7a6ce3b4fff020dec
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2021-06-22 22:11:06 +0530

    Add: DeleteBook(by id) controllers

--- controllers/books.go
+++ controllers/books.go
@@ -83,3 +83,19 @@ func UpdateBook(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"data": book})
 
 }
+
+// Delete a book
+func DeleteBook(c *gin.Context) {
+	// Get model if exist
+	var book models.Book
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+		return
+
+	}
+
+	models.DB.Delete(&book)
+
+	c.JSON(http.StatusOK, gin.H{"data": true})
+
+}
--- main.go
+++ main.go
@@ -17,6 +17,7 @@ func main() {
 	r.POST("/books", controllers.CreateBook)
 	r.GET("/books/:id", controllers.FindBook)
 	r.PATCH("/books/:id", controllers.UpdateBook)
+	r.DELETE("/books/:id", controllers.DeleteBook)
 
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"message": "hello world"})
